37cc2a0ad370918816fa0e13392313392f79e21c
HaipiYaoYao <ycyyaochengyuan@gmail.com>
2022-12-09 23:52:58 +0000

    minor changes to the code

--- .ipynb_checkpoints/log_model_spec-checkpoint.r
+++ .ipynb_checkpoints/log_model_spec-checkpoint.r
@@ -1,46 +1,27 @@
 
 library(tidyverse)
+source("conf_mat_logistic.r")
 
 ## Input: a logistic model, desiring sensitivity or specitivity 
 ##        NOTE: since this does not work for a regularized logistic model, there is an option to manually input true and pred vectors
 ## Output: a cut off number and resulting misclassification rate, sensitivity and specitivity
 
 logistic_model_sens <- function(log_model = NA, sensitivity = 0, specificity = 0, true_y = FALSE, pred_y = FALSE) {
-    
-    sens_spec <- function(newdata) { 
-        TT <- newdata %>%
-            filter(y == pred, pred == 1) %>%
-            nrow()     
-        TF <- newdata %>%
-            filter(y == pred, pred == 0) %>%
-            nrow()
-        FT <- newdata %>%
-            filter(y != pred, pred == 1) %>%
-            nrow()
-        FF <- newdata %>%
-            filter(y != pred, pred == 0) %>%
-            nrow()
-        
-        return(c(TT / (TT + FF), TF / (TF + FT), (TT + TF) / (TT + TF + FT + FF)))
-        
-        }
 
     if(sensitivity < 0 || specificity < 0 || sensitivity > 1 || specificity > 1) stop()
 
     cutoff <- 0.5
     
     if (!is.na(log_model)) {
-        tolerance_sens <- 1 / sum(model$y == 1)
-        tolerance_spec <- 1 / sum(model$y == 0)
-        data <- data.frame(y = log_model$y, pred = log_model$fitted.value)
-    } else {
-        tolerance_sens <- 1 / sum(true_y == 1)
-        tolerance_spec <- 1 / sum(true_y == 0)
-        data <- data.frame(y = true_y, pred = pred_y)
-    }
+        pred_y <- log_model$y
+        true_y <- log_model$fitted.value
+    } 
     
-    sen_and_spe <- sens_spec(data %>% mutate(pred = round(pred)))
-    result <- data.frame(Cutoff = 0.5, Sensitivity = sen_and_spe[1], Specificity = sen_and_spe[2])
+    tolerance_sens <- 1 / sum(true_y == 1)
+    tolerance_spec <- 1 / sum(true_y == 0)
+    
+    cf_info <- conf_mat_logistic(true_y = true_y, pred_y = pred_y, cutoff = 0.5, get.info = TRUE)
+    result <- data.frame(Cutoff = 0.5, Sensitivity = cf_info$sensitivity, Specificity = cf_info$specificity, Accuracy = cf_info$accuracy)
     
     if(sensitivity != 0 && specificity != 0) {
         stop("Please only specify one of the sensitivity and speciticity")
@@ -51,10 +32,9 @@ logistic_model_sens <- function(log_model = NA, sensitivity = 0, specificity = 0
         return(result)
     }
     
-
     
     if(sensitivity != 0) { 
-        sens_now <- sen_and_spe[1] 
+        sens_now <- cf_info$sensitivity 
         step <- 0.1
         last_time <- 0  
         dif <- abs(sensitivity - sens_now)
@@ -77,14 +57,14 @@ logistic_model_sens <- function(log_model = NA, sensitivity = 0, specificity = 0
                 step <- step / 10
             }
             last_time <- this_time            
-            sen_and_spe <- sens_spec(data %>% mutate(pred = round(pred - cutoff + 0.5)))       
-            sens_now <- sen_and_spe[1]
+            cf_info <- conf_mat_logistic(true_y = true_y, pred_y = pred_y, cutoff = cutoff, get.info = TRUE)
+            sens_now <- cf_info$sensitivity
             dif <- abs(sensitivity - sens_now)
         }
     }
     
     if(specificity != 0) { 
-        spec_now <- sen_and_spe[2] 
+        spec_now <- cf_info$specificity
         step <- 0.1
         last_time <- 0  
         dif <- abs(specificity - spec_now)
@@ -108,13 +88,14 @@ logistic_model_sens <- function(log_model = NA, sensitivity = 0, specificity = 0
             }
             
             last_time <- this_time            
-            sen_and_spe <- sens_spec(data %>% mutate(pred = round(pred - cutoff + 0.5)))       
-            spec_now <- sen_and_spe[2]
+            cf_info <- conf_mat_logistic(true_y = true_y, pred_y = pred_y, cutoff = cutoff, get.info = TRUE)    
+            spec_now <- cf_info$specificity
             dif <- abs(specificity - spec_now)
+                        cat("acc \n")
         }
     }
     
-    result <- data.frame(Cutoff = cutoff, Sensitivity = sen_and_spe[1], Specificity = sen_and_spe[2], Accuracy = sen_and_spe[3])
+    result <- data.frame(Cutoff = cutoff, Sensitivity = cf_info$sensitivity, Specificity = cf_info$specificity, Accuracy = cf_info$accuracy)
     return(result)
         
 }
